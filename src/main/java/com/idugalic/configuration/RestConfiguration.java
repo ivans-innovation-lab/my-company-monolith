308c876ade80db40198053a76577984d927a9bcb
Ivan Dugalic <idugalic@gmail.com>
2017-07-17 12:15:27 +0300

    Upgrading SpringBoot to 1.5.4.RELEASE
    <axon.version>3.0.4</axon.version>
         	<spring-cloud.version>Dalston.RELEASE</spring-cloud.version>
      	    <spring-boot.version>1.5.4.RELEASE</spring-boot.version>
      	    <spring-data-releasetrain.version>Ingalls-SR3</spring-data-releasetrain.version>

--- src/main/java/com/idugalic/configuration/RestConfiguration.java
+++ src/main/java/com/idugalic/configuration/RestConfiguration.java
@@ -1,8 +1,5 @@
 package com.idugalic.configuration;
 
-import com.idugalic.queryside.blog.domain.BlogPost;
-import com.idugalic.queryside.project.domain.Project;
-
 import java.lang.reflect.Method;
 
 import org.springframework.boot.autoconfigure.web.WebMvcRegistrationsAdapter;
@@ -10,11 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import com.idugalic.queryside.blog.domain.BlogPost;
+import com.idugalic.queryside.project.domain.Project;
 
 /**
  * A configuration of rest data respositories for {@link BlogPost} and {@link Project}.
@@ -25,12 +26,15 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 @Configuration
 public class RestConfiguration extends RepositoryRestMvcConfiguration {
  
-    @Override
-    protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.exposeIdsFor(BlogPost.class, Project.class);
-        config.setBasePath("/api");
-    }
     
+    @Configuration
+    static class RestConfigurationExposeId extends RepositoryRestConfigurerAdapter {
+        @Override
+        public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+            config.exposeIdsFor(BlogPost.class, Project.class);
+            config.setBasePath("/api");
+        }
+    }
 
     @Bean
     public WebMvcRegistrationsAdapter webMvcRegistrationsHandlerMapping() {
