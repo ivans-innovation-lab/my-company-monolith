f503adc0da5134c52c0e5c75ccef5e8e90bfa2e8
Ivan Dugalic <idugalic@gmail.com>
2017-09-23 22:06:52 +0200

    Removing AMQP dependency.

--- src/main/java/com/idugalic/configuration/CloudConfiguration.java
+++ src/main/java/com/idugalic/configuration/CloudConfiguration.java
@@ -1,17 +1,14 @@
 package com.idugalic.configuration;
 
-
 import javax.sql.DataSource;
 
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
-public class CloudConfiguration extends AbstractCloudConfig{
-    
-    @Bean
-    public DataSource dataSource() {
-        return connectionFactory().dataSource();
-    }
+public class CloudConfiguration extends AbstractCloudConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		return connectionFactory().dataSource();
+	}
 }
