274180718759976e801535a56ca474fc94de0d6e
Ivan Dugalic <ivan.dugalic@namics.com>
2017-10-29 17:51:27 +0100

    Fixing the CircleCI build
    It was deleting the jar file.

--- src/main/java/com/idugalic/configuration/CloudConfiguration.java
+++ src/main/java/com/idugalic/configuration/CloudConfiguration.java
@@ -2,9 +2,11 @@ package com.idugalic.configuration;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 
+@EntityScan
 public class CloudConfiguration extends AbstractCloudConfig {
 
 	@Bean
