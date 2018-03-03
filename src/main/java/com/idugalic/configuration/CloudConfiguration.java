05c6cccfb93ff5eb84f8def9c914609516d50b5f
Ivan Dugalic <ivan.dugalic@namics.com>
2018-03-03 17:56:57 +0100

    Removing cloud specific configuration

--- src/main/java/com/idugalic/configuration/CloudConfiguration.java
+++ src/main/java/com/idugalic/configuration/CloudConfiguration.java
@@ -1,16 +1,13 @@
 package com.idugalic.configuration;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
-import org.springframework.context.annotation.Bean;
 
 @EntityScan
 public class CloudConfiguration extends AbstractCloudConfig {
 
-	@Bean
-	public DataSource dataSource() {
-		return connectionFactory().dataSource();
-	}
+//	@Bean
+//	public DataSource dataSource() {
+//		return connectionFactory().dataSource();
+//	}
 }
