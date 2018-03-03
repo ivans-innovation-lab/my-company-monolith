e8ad777ea2ef6c67ea867a2bf3f87be891c2e52b
Ivan Dugalic <ivan.dugalic@namics.com>
2018-03-03 19:06:53 +0100

    Configuring entityManagerFactory

--- src/main/java/com/idugalic/configuration/CloudConfiguration.java
+++ src/main/java/com/idugalic/configuration/CloudConfiguration.java
@@ -2,20 +2,21 @@ package com.idugalic.configuration;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
-@EntityScan
+@Configuration
 public class CloudConfiguration extends AbstractCloudConfig {
 
 	@Bean
 	public DataSource dataSource() {
 		return connectionFactory().dataSource();
 	}
+	
 	@Bean(name="entityManagerFactory")
-	public LocalContainerEntityManagerFactoryBean getEntityManagerFactoryBean() {
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 	    return new LocalContainerEntityManagerFactoryBean();
 }
 }
